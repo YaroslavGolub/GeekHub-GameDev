73e0f42056c550cc0987f2cab58ba11dc1fb86fb
neverfo <neverfo@gmail.com>
2016-12-04 16:54:13 +0200

    script changes from Task2

---
+++ Assets/Scripts/Task2/PlayerMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Task2
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField]
+        protected float PlayerSpeed;
+
+        private float _rotation;
+
+        private Vector3 _moveDirection;
+
+        private void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            _moveDirection.z = Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime;
+            _rotation = Input.GetAxis("Horizontal");
+
+            transform.Translate(_moveDirection);
+            transform.Rotate(0, _rotation, 0);
+        }
+    }
+}
